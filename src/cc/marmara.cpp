28a8a48409e622ea1e9fb5923fe8e3c5a7334c4e
dimxy <dimxy@komodoplatform.com>
2020-08-18 20:45:08 +0300

    fix compile err

--- src/cc/marmara.cpp
+++ src/cc/marmara.cpp
@@ -5928,7 +5928,7 @@ UniValue MarmaraAddressAmountStat()
     std::vector<std::pair<CAddressUnspentKey, CAddressUnspentValue> > unspentOutputs;
 
     CAmount normals = 0LL;
-    CAmount ppsh = 0LL
+    CAmount ppsh = 0LL;
     CAmount lcl = 0LL;
     CAmount activated = 0LL;
     CAmount ccunk = 0LL;
