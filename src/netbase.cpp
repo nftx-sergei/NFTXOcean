137bcc0d77d5d133264703d34fbd68618bd77918
DeckerSU <deckersu@protonmail.com>
2022-02-07 16:13:21 +0300

    net: add an internal subnet for representing unresolved hostnames
    backport of https://github.com/bitcoin/bitcoin/commit/7f31762cb6261806542cc6d1188ca07db98a6950
    in our codebase.
    
    TODO: consider backport of https://github.com/bitcoin/bitcoin/pull/10446
    and all of its commits, not only  "add an internal subnet".

--- src/netbase.cpp
+++ src/netbase.cpp
@@ -65,6 +65,7 @@ int nConnectTimeout = DEFAULT_CONNECT_TIMEOUT;
 bool fNameLookup = false;
 
 static const unsigned char pchIPv4[12] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff };
+static const unsigned char pchOnionCat[] = {0xFD,0x87,0xD8,0x7E,0xEB,0x43};
 
 // 0xFD + sha256("bitcoin")[0:5]
 static const unsigned char g_internal_prefix[] = { 0xFD, 0x6B, 0x88, 0xC0, 0x87, 0x24 };
@@ -664,6 +665,32 @@ void CNetAddr::Init()
     memset(ip, 0, sizeof(ip));
 }
 
+bool CNetAddr::SetInternal(const std::string &name)
+{
+    if (name.empty()) {
+        return false;
+    }
+    unsigned char hash[32] = {};
+    CSHA256().Write((const unsigned char*)name.data(), name.size()).Finalize(hash);
+    memcpy(ip, g_internal_prefix, sizeof(g_internal_prefix));
+    memcpy(ip + sizeof(g_internal_prefix), hash, sizeof(ip) - sizeof(g_internal_prefix));
+    return true;
+}
+
+bool CNetAddr::SetSpecial(const std::string &strName)
+{
+    if (strName.size()>6 && strName.substr(strName.size() - 6, 6) == ".onion") {
+        std::vector<unsigned char> vchAddr = DecodeBase32(strName.substr(0, strName.size() - 6).c_str());
+        if (vchAddr.size() != 16-sizeof(pchOnionCat))
+            return false;
+        memcpy(ip, pchOnionCat, sizeof(pchOnionCat));
+        for (unsigned int i=0; i<16-sizeof(pchOnionCat); i++)
+            ip[i + sizeof(pchOnionCat)] = vchAddr[i];
+        return true;
+    }
+    return false;
+}
+
 void CNetAddr::SetIP(const CNetAddr& ipIn)
 {
     memcpy(ip, ipIn.ip, sizeof(ip));
@@ -683,22 +710,6 @@ void CNetAddr::SetRaw(Network network, const uint8_t *ip_in)
         default:
             assert(!"invalid network");
     }
-}
-
-static const unsigned char pchOnionCat[] = {0xFD,0x87,0xD8,0x7E,0xEB,0x43};
-
-bool CNetAddr::SetSpecial(const std::string &strName)
-{
-    if (strName.size()>6 && strName.substr(strName.size() - 6, 6) == ".onion") {
-        std::vector<unsigned char> vchAddr = DecodeBase32(strName.substr(0, strName.size() - 6).c_str());
-        if (vchAddr.size() != 16-sizeof(pchOnionCat))
-            return false;
-        memcpy(ip, pchOnionCat, sizeof(pchOnionCat));
-        for (unsigned int i=0; i<16-sizeof(pchOnionCat); i++)
-            ip[i + sizeof(pchOnionCat)] = vchAddr[i];
-        return true;
-    }
-    return false;
 }
 
 CNetAddr::CNetAddr()
@@ -745,7 +756,7 @@ bool CNetAddr::IsIPv4() const
 
 bool CNetAddr::IsIPv6() const
 {
-    return (!IsIPv4() && !IsTor());
+    return (!IsIPv4() && !IsTor() && !IsInternal());
 }
 
 bool CNetAddr::IsRFC1918() const
@@ -866,6 +877,9 @@ bool CNetAddr::IsValid() const
     if (IsRFC3849())
         return false;
 
+    if (IsInternal())
+        return false;
+
     if (IsIPv4())
     {
         // INADDR_NONE
@@ -884,7 +898,7 @@ bool CNetAddr::IsValid() const
 
 bool CNetAddr::IsRoutable() const
 {
-    return IsValid() && !(IsRFC1918() || IsRFC2544() || IsRFC3927() || IsRFC4862() || IsRFC6598() || IsRFC5737() || (IsRFC4193() && !IsTor()) || IsRFC4843() || IsLocal());
+    return IsValid() && !(IsRFC1918() || IsRFC2544() || IsRFC3927() || IsRFC4862() || IsRFC6598() || IsRFC5737() || (IsRFC4193() && !IsTor()) || IsRFC4843() || IsLocal() || IsInternal());
 }
 
 bool CNetAddr::IsInternal() const
@@ -894,6 +908,9 @@ bool CNetAddr::IsInternal() const
 
 enum Network CNetAddr::GetNetwork() const
 {
+    if (IsInternal())
+        return NET_INTERNAL;
+
     if (!IsRoutable())
         return NET_UNROUTABLE;
 
@@ -910,6 +927,8 @@ std::string CNetAddr::ToStringIP() const
 {
     if (IsTor())
         return EncodeBase32(&ip[6], 10) + ".onion";
+    if (IsInternal())
+        return EncodeBase32(ip + sizeof(g_internal_prefix), sizeof(ip) - sizeof(g_internal_prefix)) + ".internal";
     CService serv(*this, 0);
     struct sockaddr_storage sockaddr;
     socklen_t socklen = sizeof(sockaddr);
@@ -1055,8 +1074,15 @@ std::vector<unsigned char> CNetAddr::GetGroup(const std::vector<bool> &asmap) co
         nBits = 0;
     }
 
-    // all unroutable addresses belong to the same group
-    if (!IsRoutable())
+    // all internal-usage addresses get their own group
+    if (IsInternal())
+    {
+        nClass = NET_INTERNAL;
+        nStartByte = sizeof(g_internal_prefix);
+        nBits = (sizeof(ip) - sizeof(g_internal_prefix)) * 8;
+    }
+    // all other unroutable addresses belong to the same group
+    else if (!IsRoutable())
     {
         nClass = NET_UNROUTABLE;
         nBits = 0;
@@ -1142,7 +1168,7 @@ int CNetAddr::GetReachabilityFrom(const CNetAddr *paddrPartner) const
         REACH_PRIVATE
     };
 
-    if (!IsRoutable())
+    if (!IsRoutable() || IsInternal())
         return REACH_UNREACHABLE;
 
     int ourNet = GetExtNetwork(this);
@@ -1333,7 +1359,7 @@ std::string CService::ToStringPort() const
 
 std::string CService::ToStringIPPort() const
 {
-    if (IsIPv4() || IsTor()) {
+    if (IsIPv4() || IsTor() || IsInternal()) {
         return ToStringIP() + ":" + ToStringPort();
     } else {
         return "[" + ToStringIP() + "]:" + ToStringPort();
--- src/test-komodo/test_netbase_tests.cpp
+++ src/test-komodo/test_netbase_tests.cpp
@@ -57,9 +57,16 @@ static CNetAddr ResolveIP(const std::string& ip)
         return addr;
 }
 
+static CNetAddr CreateInternal(const char* host)
+{
+    CNetAddr addr;
+    addr.SetInternal(host);
+    return addr;
+}
+
 namespace TestNetBaseTests {
 
-    TEST(TestAddrmanTests, netbase_getgroup) {
+    TEST(TestNetBaseTests, netbase_getgroup) {
 
         std::vector<bool> asmap; // use /16
         ASSERT_TRUE(ResolveIP("127.0.0.1").GetGroup(asmap) == std::vector<unsigned char>({0})); // Local -> !Routable()
@@ -79,5 +86,57 @@ namespace TestNetBaseTests {
         ASSERT_TRUE(ResolveIP("2001:470:abcd:9999:9999:9999:9999:9999").GetGroup(asmap) == std::vector<unsigned char>({(unsigned char)NET_IPV6, 32, 1, 4, 112, 175})); //he.net
         ASSERT_TRUE(ResolveIP("2001:2001:9999:9999:9999:9999:9999:9999").GetGroup(asmap) == std::vector<unsigned char>({(unsigned char)NET_IPV6, 32, 1, 32, 1})); //IPv6
 
+        // baz.net sha256 hash: 12929400eb4607c4ac075f087167e75286b179c693eb059a01774b864e8fe505
+        std::vector<unsigned char> internal_group = {NET_INTERNAL, 0x12, 0x92, 0x94, 0x00, 0xeb, 0x46, 0x07, 0xc4, 0xac, 0x07};
+        EXPECT_TRUE(CreateInternal("baz.net").GetGroup(asmap) == internal_group);
+
+        // dummy "ip" for this association with a prefix of fd6b:88c0:8724::/48 (fd + sha256(bitcoin)[0:5])
+        std::vector<unsigned char> internal_group_test = {NET_INTERNAL, 0x18, 0x75, 0x13, 0xd6, 0x8a, 0x23, 0x70, 0xfd, 0x4a, 0x5d};
+        CNetAddr dummyAddr = CreateInternal("decker.dummy");
+        EXPECT_TRUE(dummyAddr.GetGroup(asmap) == internal_group_test);
+        EXPECT_EQ(std::string("db2rhvukenyp2ss5.internal"), dummyAddr.ToStringIP());
+    }
+
+    TEST(TestNetBaseTests, netbase_networks) {
+
+        EXPECT_TRUE(ResolveIP("127.0.0.1").GetNetwork()                              == NET_UNROUTABLE);
+        EXPECT_TRUE(ResolveIP("::1").GetNetwork()                                    == NET_UNROUTABLE);
+        EXPECT_TRUE(ResolveIP("8.8.8.8").GetNetwork()                                == NET_IPV4);
+        EXPECT_TRUE(ResolveIP("2001::8888").GetNetwork()                             == NET_IPV6);
+        EXPECT_TRUE(ResolveIP("FD87:D87E:EB43:edb1:8e4:3588:e546:35ca").GetNetwork() == NET_ONION);
+        EXPECT_TRUE(CreateInternal("foo.com").GetNetwork()                           == NET_INTERNAL);
+
+    }
+
+    TEST(TestNetBaseTests, netbase_properties) {
+        EXPECT_TRUE(CNetAddr("127.0.0.1").IsIPv4());
+        EXPECT_TRUE(CNetAddr("::FFFF:192.168.1.1").IsIPv4());
+        EXPECT_TRUE(CNetAddr("::1").IsIPv6());
+        EXPECT_TRUE(CNetAddr("10.0.0.1").IsRFC1918());
+        EXPECT_TRUE(CNetAddr("192.168.1.1").IsRFC1918());
+        EXPECT_TRUE(CNetAddr("172.31.255.255").IsRFC1918());
+        EXPECT_TRUE(CNetAddr("2001:0DB8::").IsRFC3849());
+        EXPECT_TRUE(CNetAddr("169.254.1.1").IsRFC3927());
+        EXPECT_TRUE(CNetAddr("2002::1").IsRFC3964());
+        EXPECT_TRUE(CNetAddr("FC00::").IsRFC4193());
+        EXPECT_TRUE(CNetAddr("2001::2").IsRFC4380());
+        EXPECT_TRUE(CNetAddr("2001:10::").IsRFC4843());
+        EXPECT_TRUE(CNetAddr("FE80::").IsRFC4862());
+        EXPECT_TRUE(CNetAddr("64:FF9B::").IsRFC6052());
+        EXPECT_TRUE(CNetAddr("FD87:D87E:EB43:edb1:8e4:3588:e546:35ca").IsTor());
+        EXPECT_TRUE(CNetAddr("127.0.0.1").IsLocal());
+        EXPECT_TRUE(CNetAddr("::1").IsLocal());
+        EXPECT_TRUE(CNetAddr("8.8.8.8").IsRoutable());
+        EXPECT_TRUE(CNetAddr("2001::1").IsRoutable());
+        EXPECT_TRUE(CNetAddr("127.0.0.1").IsValid());
+        EXPECT_TRUE(CreateInternal("FD6B:88C0:8724:edb1:8e4:3588:e546:35ca").IsInternal());
+        EXPECT_TRUE(CreateInternal("bar.com").IsInternal());
+    }
+
+    TEST(TestNetBaseTests, embedded_test) {
+        CNetAddr addr1(ResolveIP("1.2.3.4"));
+        CNetAddr addr2(ResolveIP("::FFFF:0102:0304"));
+        EXPECT_TRUE(addr2.IsIPv4());
+        EXPECT_EQ(addr1.ToString(), addr2.ToString());
     }
 }
